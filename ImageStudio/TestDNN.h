9d6d370c43eff1c55c39c6ecbf1a6a293123c5bb
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2021-01-19 10:48:11 +0800

    添加了音频处理；Opencv DNN模块；自定义layer

---
+++ ImageStudio/TestDNN.h
@@ -0,0 +1,11 @@
+#pragma once
+class TestDNN
+{
+public:
+	/// <summary>
+	/// HED: 基于深度学习的边缘检测
+	/// </summary>
+	/// <param name="filepath"></param>
+	static void TestHED(const char* filepath);
+};
+
