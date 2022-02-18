b576d3dd59090dfe87607ecc3cd0c76816608006
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-02-18 15:06:56 +0800

    Python测试

---
+++ PythonExam/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PythonExam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonRunClick(object sender, RoutedEventArgs e)
+        {
+            //执行脚本
+            try
+            {
+                ScriptEngine engine = Python.CreateEngine();
+                ScriptScope scope = engine.CreateScope();
+                var scriptIO = engine.Runtime.IO;
+
+                //var ms = new System.IO.MemoryStream();
+                //var tw = new System.IO.StringWriter();
+                //scriptIO.SetOutput(ms, tw);
+                var stream = new TriggerStream();  // TriggerStream是一个能够在写入时触发事件的, 继承了Stream的类.
+                stream.PreviewWrite += (ss, ee) => {
+                    if(ee.Count > 0)
+                    {
+                        var data = new byte[ee.Count];
+                        Array.Copy(ee.Buffer, ee.Offset, data, 0, ee.Count);
+                        var s = Encoding.Default.GetString(data);
+                        if (s != null)
+                        {
+                            this.Dispatcher.Invoke(new Action(() => {
+                                textMessage.AppendText(s);
+                                textMessage.AppendText("\r\n");
+                            }));
+                        }
+                    }
+                };
+                engine.Runtime.IO.SetOutput(stream, Encoding.Default);  // 这样, 我们可以通过TriggerStream的写入事件来获取写入的内容
+                engine.Runtime.IO.SetErrorOutput(stream, Encoding.Default);
+
+                //
+                //scriptIO.RedirectToConsole();
+
+                //ScriptSource script = engine.CreateScriptSourceFromFile(@"Script.py");
+                var script = engine.CreateScriptSourceFromString(textScript.Text);
+
+                var result = script.Execute(scope);
+
+                var names = scope.GetVariableNames();
+                
+
+                if (result != null)
+                {
+                    textMessage.AppendText("返回结果为: " + result.ToString());
+                    textMessage.AppendText("\r\n");
+                }
+                else
+                {
+                    textMessage.AppendText("result为null");
+                    textMessage.AppendText("\r\n");
+                }
+                //tw.Flush();
+                //var text = tw.ToString();
+            }
+            catch (Exception exp)
+            {
+                textMessage.AppendText(exp.Message);
+                textMessage.AppendText("\r\n");
+            }
+        }
+    }
+}
---
+++ PythonExam/TriggerStream.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PythonExam
+{
+    class WriteStreamEventArgs : EventArgs
+    {
+        public byte[] Buffer;
+        public int Offset, Count;
+    }
+
+    /// <summary>
+    /// 来自项目 
+    /// https://github.com/SlimeNull/Null.TextEditor
+    /// </summary>
+    class TriggerStream : Stream
+    {
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => 0;
+
+        public override long Position { get => 0; set { } }
+
+        public override void Flush() { }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return 0;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return 0;
+        }
+
+        public override void SetLength(long value) { }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (PreviewWrite != null)
+                PreviewWrite.Invoke(this, new WriteStreamEventArgs()
+                {
+                    Buffer = buffer,
+                    Offset = offset,
+                    Count = count,
+                });
+        }
+        public event EventHandler<WriteStreamEventArgs> PreviewWrite;
+    }
+}
