0e5c05b6742c08f376f5705e5adc7d30e5256812
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-02-25 15:24:43 +0800

    一些工具函数

---
+++ Taiji.Utils/InMemoryFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taiji.Utils
+{
+    /// <summary>
+    /// 用于文件压缩
+    /// 也可用于其他地方，文件名和文件内容
+    /// https://stackoverflow.com/questions/53378497/net-core-create-on-memory-zipfile
+    /// </summary>
+    public class InMemoryFile
+    {
+        public string FileName { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
