1b5407d703fd04cd4e0d710d1248d7d1060ac284
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-02-25 14:30:37 +0800

    UI

--- AIStudioClient.Modules.TorchPlatform/Views/TorchMainWnd.xaml.cs
+++ AIStudioClient.Modules.TorchPlatform/Views/TorchMainWnd.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using ArtificialIntelligenceStudioClient.Core;
+using ArtificialIntelligenceStudioClient.Core.ViewModels;
+using Microsoft.Extensions.Logging;
+using Prism.Commands;
+using Prism.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +25,68 @@ namespace AIStudioClient.Modules.TorchPlatform.Views
     /// </summary>
     public partial class TorchMainWnd : UserControl
     {
-        public TorchMainWnd()
+        public TorchMainWnd(ILogger logger, IEventAggregator eventAggregator, TorchPlatformModule module)
         {
             InitializeComponent();
+            _logger = logger;
+            _eventAggregator = eventAggregator;
+            _module = module;
+            InitUI();
+            BindEvents();
         }
+
+        #region 事件处理
+        private void OnWndLoaded(object sender, RoutedEventArgs e)
+        {
+            var args = new ModuleLoadedEventArgs()
+            {
+                Title = _module.Title,
+                Key = typeof(TorchPlatformModule).Name,
+            };
+            args.Buttons.AddRange(_buttons);
+            _eventAggregator.GetEvent<ModuleLoadedEvent>().Publish(args);
+        }
+
+        private void OnWndUnloaded(object sender, RoutedEventArgs e)
+        {
+            var args = new ModuleUnloadEventArgs()
+            {
+                Key = typeof(TorchPlatformModule).Name,
+            };
+            _eventAggregator.GetEvent<ModuleUnloadEvent>().Publish(args);
+        }
+
+        private void OnCommandBPNet()
+        {
+        }
+
+        #endregion
+
+        #region 辅助函数
+        ILogger _logger;
+        IEventAggregator _eventAggregator;
+        TorchPlatformModule _module;
+        RibbonButtonViewModel? _buttonBPNet = null;
+        DelegateCommand? _delegateCommandSelectImageFolder;
+        List<RibbonButtonViewModel> _buttons = new List<RibbonButtonViewModel>();
+
+        void InitUI()
+        {
+            _delegateCommandSelectImageFolder = new DelegateCommand(OnCommandBPNet);
+            _buttonBPNet = new RibbonButtonViewModel()
+            {
+                Title = "BP神经网络",
+                Description = "BP神经网络",
+                Command = _delegateCommandSelectImageFolder,
+            };
+            _buttons.Add(_buttonBPNet);
+        }
+
+        void BindEvents()
+        {
+            this.Loaded += OnWndLoaded;
+            this.Unloaded += OnWndUnloaded;
+        }
+        #endregion
     }
 }
