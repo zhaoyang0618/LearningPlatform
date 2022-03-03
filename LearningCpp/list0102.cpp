acec283efcb27b8c29a1966c6aceba172486b3ff
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-03-03 15:24:34 +0800

    std::format函数的一些用法

--- LearningCpp/list0102.cpp
+++ LearningCpp/list0102.cpp
@@ -18,6 +18,8 @@
 #include <string>
 #include <system_error>
 #include <vector>
+#include <iomanip>
+#include <format>
 
 //import <algorithm>;
 //import <fstream>;
@@ -93,6 +95,8 @@ try
 	// Part 3. Print the sorted text.
 	for (auto const& line : text)
 		std::cout << line << '\n';
+
+	return EXIT_SUCCESS;
 }
 catch (std::exception& ex)
 {
@@ -105,3 +109,45 @@ catch (...)
 	std::cerr << "Caught unknown exception type.\nTerminating program.\n";
 	std::exit(EXIT_FAILURE);
 }
+
+
+/// <summary>
+/// 输出格式的控制
+/// </summary>
+void testFormattedOut()
+{
+	std::cout << " N N^2 N^3\n";
+	for (int i{ 1 }; i != 21; ++i)
+		std::cout << std::setw(2) << i
+		<< std::setw(6) << i * i
+		<< std::setw(7) << i * i * i
+		<< '\n';
+
+	std::cout << std::format("x: {}, y: {}, z: {}\n", 10, 20, 30) << std::endl;
+	std::cout << std::format("x: {2}, y: {1}, z: {0}\n", 10, 20, 30) << std::endl;
+	std::cout << std::format("'{0:c}': {0:#04x} {0:0>#10b} |{0:{1}d}| {2:s}\n", '*', 4, "str") << std::endl;
+
+	//
+	int constexpr low{ 1 }; ///< Minimum value for the table
+	int constexpr high{ 10 }; ///< Maximum value for the table
+	int constexpr colwidth{ 4 }; ///< Fixed width for all columns
+	// First print the header.
+	std::cout << std::format("{1:>{0}c}|", colwidth, '*');
+	for (int i{ low }; i <= high; i = i + 1)
+		std::cout << std::format("{1:{0}}", colwidth, i);
+	std::cout << '\n';
+	// Print the table rule by using the fill character.
+	std::cout << std::format("{2:->{0}}+{2:->{1}}\n",
+		colwidth, (high - low + 1) * colwidth, "");
+	// For each row...
+	for (int row{ low }; row <= high; row = row + 1)
+	{
+		std::cout << std::format("{1:{0}}|", colwidth, row);
+		// Print all the columns.
+		for (int col{ low }; col <= high; col = col + 1)
+			std::cout << std::format("{1:{0}}", colwidth, row * col);
+		std::cout << '\n';
+	}
+
+}
+
