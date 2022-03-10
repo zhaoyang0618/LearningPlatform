79e90556b7962f642a1e9dd4d25b30a32097491d
zhaoyang0618 <zhaoyang0618@msn.com>
2022-03-10 21:19:15 +0800

    标注的对象

---
+++ AIStudioClient.Modules.ImageAnnotation/ViewModels/AnnotationObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIStudioClient.Modules.ImageAnnotation.ViewModels
+{
+    /// <summary>
+    /// 需要标注的对象
+    /// </summary>
+    public class AnnotationObject
+    {
+        /// <summary>
+        /// 唯一
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 这一次是否需要被用到
+        /// </summary>
+        public int isUsed { get; set; }
+    }
+}
