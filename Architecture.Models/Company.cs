88bf7747a79b58bf902d9ca191dc56cbd32db52f
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-09-26 10:53:38 +0800

    建筑基础数据

---
+++ Architecture.Models/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace Architecture.Models
+{
+    /// <summary>
+    /// 建筑企业
+    /// </summary>
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
