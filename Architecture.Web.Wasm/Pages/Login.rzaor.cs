97f0403b3401f1e3e54feef690c5fcd21b5b165e
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-09-28 15:39:25 +0800

    Blazor

---
+++ Architecture.Web.Wasm/Pages/Login.rzaor.cs
@@ -0,0 +1,23 @@
+﻿using Caviar.SharedKernel.Entities.User;
+
+namespace Architecture.Web.Wasm.Pages
+{
+    public partial class Login
+    {
+        public UserLogin ApplicationUser { get; set; } = new UserLogin()
+        {
+            UserName = "admin",
+            Password = "123456",
+            RememberMe = true,
+        };
+
+        string? style;
+
+        protected override void OnInitialized()
+        {
+            string backgroundImage = "_content/Caviar.AntDesignUI/images/grov.jpg";
+            style = $"min-height:100vh;background-image: url({backgroundImage});";
+            base.OnInitialized();
+        }
+    }
+}
