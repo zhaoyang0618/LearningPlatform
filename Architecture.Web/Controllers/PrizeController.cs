b66b343af41e10efed7edcd6cbd877f25c4a748a
zhaoyang0618 <zhaoyang0618@users.noreply.github.com>
2022-10-13 09:27:53 +0800

    重新摸索Caviar框架

---
+++ Architecture.Web/Controllers/PrizeController.cs
@@ -0,0 +1,17 @@
+﻿using Architecture.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Architecture.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PrizeController : ControllerBase
+    {
+        [HttpGet]
+        public List<Prize> List()
+        {
+            return new List<Prize>() { new Prize() { Id=1, Name="AAA" }, new Prize() { Id=2, Name="BBBBBB" } };
+        }
+    }
+}
